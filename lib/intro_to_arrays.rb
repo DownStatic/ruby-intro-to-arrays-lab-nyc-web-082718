868531d93868bd129d6d1fec2658479ecdd0ac42
Christopher Stull <chris.stull@gmail.com>
2018-08-14 17:23:57 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,12 +18,12 @@ def last_element(a1)
   return a1[-1]
 end
 
-def first_element_with_array(a1)
+def first_element_with_array_methods(a1)
   first = a1.shift()
   return first
 end
 
-def last_element_with_array(a1)
+def last_element_with_array_methods(a1)
   last = a1.pop()
   return last
 end

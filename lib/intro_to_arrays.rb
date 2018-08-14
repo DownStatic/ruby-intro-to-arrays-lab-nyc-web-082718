4be236f85f859a58c4b553062fd8f1290558bd3e
Christopher Stull <chris.stull@gmail.com>
2018-08-14 17:18:32 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  return []
+end
+
+def array_with_two_elements(e1,e2)
+  return [e1,e2]
+end
+
+def first_element(a1)
+  return a1[0]
+end
+
+def third_element(a1)
+  return a1[2]
+end
+
+def last_element(a1)
+  return a1[-1]
+end
+
+def first_element_with_array(a1)
+  return a1.shift()
+end
+
+def last_element_with_array(a1)
+  return a1.pop()
+end
+
+def length_of_array(a1)
+  return a1.length
+end
